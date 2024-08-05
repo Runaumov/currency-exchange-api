e6cfa70aa26abbd97d44f2abbcab1e41f3a8d06d
Runaumov <casrunau@gmail.com>
2024-08-05 22:57:09 +0300

    Refactoring some DTO-classes

--- src/main/java/org/example/currencyexchangeapi/dto/ResponseConversionDto.java
+++ src/main/java/org/example/currencyexchangeapi/dto/ResponseConversionDto.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.currencyexchangeapi.model.Currency;
-
 import java.math.BigDecimal;
 
 @AllArgsConstructor
