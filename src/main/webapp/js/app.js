e3b10448d8d423fd67147bb3e493a4a0ce61e07f
Runaumov <casrunau@gmail.com>
2024-08-07 00:59:27 +0300

    change ip-adress

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const host = "http://localhost:8080/currency_exchange_api"
+    const host = "http://localhost:8080/currency_exchange_api/"
 
     // Fetch the list of currencies and populate the select element
     function requestCurrencies() {
